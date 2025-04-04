40ff754a9fdff9a18802df352b9dbe30ab7ace37
castle-murray <sean@castle-murray.com>
2025-04-04 00:16:46 -0400

    wsgi.py update

--- callman/wsgi.py
+++ callman/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+import dotenv
 
+
+
+dotenv.read_dotenv()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'callman.settings')
 
 application = get_wsgi_application()
