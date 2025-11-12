653559aca79f44f3b1f0e3f5a7e774e0a372c58d
castle-murray <sean@castle-murray.com>
2025-11-12 10:28:41 -0500

    secret key in settings.py

--- callman/settings.py
+++ callman/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*xv&h_8$1p2+klqa+2sz#&dj3*(z3cs2i&y6)0k#9pg*bs3biu'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get('DEBUG') == 'True':
