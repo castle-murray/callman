f7f47f17a6101883185fb59b5b976b97ebd5cc0f
castlemurray <sean@castle-murray.com>
2026-01-25 13:46:02 -0500

    Revert run_daphne.py to original state

--- run_daphne.py
+++ run_daphne.py
@@ -10,13 +10,8 @@ load_dotenv()
 # Set Django settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'callman.settings')
 
-# Now import and run Daphne with auto-reload
+# Now import and run Daphne
 from daphne.cli import CommandLineInterface
-import hupper
-
-def run_daphne():
-    CommandLineInterface.entrypoint()
 
 if __name__ == '__main__':
-    reloader = hupper.start_reloader(run_daphne)
-    run_daphne()
+    CommandLineInterface.entrypoint()
